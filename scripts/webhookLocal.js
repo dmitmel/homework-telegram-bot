1c3d759b017bbb28bb032dc6d85cfc73d48356bc
dmitmel <dmytro.meleshko@gmail.com>
2018-04-16 21:32:37 +0300

    add cleanup on exit

--- scripts/webhookLocal.js
+++ scripts/webhookLocal.js
@@ -1,19 +1,23 @@
 const chalk = require('chalk').default;
 const localtunnel = require('localtunnel');
 const logger = require('../lib/logger');
+const { onExit } = require('../lib/exit');
 const getEnv = require('../lib/utils/getEnv');
 const { startWebhook } = require('../lib/');
 
 const port = getEnv('WEBHOOK_PORT', 3000);
 
-logger.debug('creating local tunnel');
+logger.info('creating local tunnel');
 const tunnel = localtunnel(port, (error, tunnelInfo) => {
   if (error) throw error;
 
   const { url } = tunnelInfo;
 
-  logger.debug('local tunnel created: %s', chalk.cyan(url));
+  logger.info('local tunnel created: %s', chalk.cyan(url));
   startWebhook(url, port);
 });
 
-tunnel.on('close', () => logger.debug('closing local tunnel'));
+onExit(() => {
+  logger.info('closing local tunnel');
+  tunnel.close();
+});
