8a40168991f0fea08ad2f2d8e7b8112ff0b2214d
dmitmel <dmytro.meleshko@gmail.com>
2018-04-20 18:44:08 +0300

    [exit] remove 'exit' function

--- scripts/webhookLocal.js
+++ scripts/webhookLocal.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk').default;
 const localtunnel = require('localtunnel');
 const logger = require('../lib/logger');
-const { onExit } = require('../lib/exit');
+const onExit = require('../lib/onExit');
 const getEnv = require('../lib/utils/getEnv');
 const Bot = require('../lib/bot');
 
